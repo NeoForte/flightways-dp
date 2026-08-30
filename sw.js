f2f6f89e01ea6bd6565e5e3fea2f3aa48497c8bf
NeoForte <neoforte@protonmail.com>
2026-08-30 10:29:00 -0400

    Add files via upload
    1.2

--- sw.js
+++ sw.js
@@ -1,1 +1,19 @@
-const CACHE='flightways-dp-v1.1';const ASSETS=['./','./index.html','./manifest.webmanifest','./flightways-columbus-logo.png'];self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+const CACHE='flightways-dp-v1.2';
+const ASSETS=[
+  './',
+  './index.html',
+  './manifest.webmanifest',
+  './flightways-columbus-logo.png',
+  './dp-icon-192.png',
+  './dp-icon-512.png',
+  './apple-touch-icon.png',
+  './favicon-32.png'
+];
+self.addEventListener('install',e=>{
+  self.skipWaiting();
+  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
+});
+self.addEventListener('activate',e=>{
+  e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));
+});
+self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
